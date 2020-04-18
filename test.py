9f4141632513a9a764e1c8f7fb37116897c9b36d
The1Penguin <jfuhr01@gmail.com>
2020-04-18 23:28:28 +0200

    Changed code from saying test to listing the first 100 primes

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print("Test")
+import math
+def checkprime(primes, check):
+    for i in primes:
+        if i > math.sqrt(check):
+            break
+        if check % i == 0:
+            return None
+
+    return check
+
+primer = []
+number = 2
+while len(primer) < 100:
+
+    a = checkprime(primer,number)
+    if not (a == None):
+        primer.append(a)
+    number += 1
+
+print(primer)
